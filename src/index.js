39523803cefed887e84c9ecf987048768f29a7ca
shinkare90 <55100900+Shinkarev90@users.noreply.github.com>
2021-03-14 21:36:30 +0400

    feat: add human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function toReadable (number) {
-  
+    const num0 = ['zero'];
+    const numone = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const numtwenty = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const num = ['hundred'];
+    let n = number;
+    if (n == 0) {
+        return num0[0];
+    } else if (n < 20) {
+        let i = n;
+        return numone[i]
+    } else if (n < 100 && n%10 == 0) {
+        let str = String(n);
+        let arr = str.split('');
+        return numtwenty[arr[0]];
+    } else if (n < 100) {
+        let str = String(n);
+        let arr = str.split('');
+        return numtwenty[arr[0]] + ' ' + numone[arr[1]];
+    } else if (n%100 == 0) {
+        let str = String(n);
+        let arr = str.split('');
+        return numone[arr[0]] + ' ' + num[0];
+    } else if (n > 100 && n < 120 || n > 200 && n < 220 || n > 300 && n < 320 || n > 400 && n < 420 || n > 500 && n < 520 || n > 600 && n < 620 || n > 700 && n < 720 || n > 800 && n < 820 || n > 900 && n < 920) {
+        let str = String(n);
+        let arr = str.split('');
+        let h = n%100;
+        console.log(h);
+        return numone[arr[0]] + ' ' + num[0] + ' ' + numone[h];
+    } else if (n%10 == 0) {
+        let str = String(n);
+        let arr = str.split('');
+        return numone[arr[0]] + ' ' + num[0] + ' ' + numtwenty[arr[1]];
+    } else if (n < 1000) {
+        let str = String(n);
+        let arr = str.split('');
+        return numone[arr[0]] + ' ' + num[0] + ' ' + numtwenty[arr[1]] + ' ' + numone[arr[2]];
+    }
 }
